60ab364c49dfdf378425aa6c7e227580b4d55f13
nkh1006 <mqsora@naver.com>
2024-01-05 00:06:28 +0900

    lesson

---
+++ book/30p.js
@@ -0,0 +1,11 @@
+function solution(m, n, current) {
+  if (current >= n) {
+    return 0;
+  } else if (current < m) {
+    return 1 + solution(m, n, current * 2);
+  } else {
+    return 1 + solution(m, n, current + m);
+  }
+}
+
+solution(3, 20, 1);
